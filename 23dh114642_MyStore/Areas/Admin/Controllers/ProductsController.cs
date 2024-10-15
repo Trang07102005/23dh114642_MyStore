ab13953864c988aa984a336475704d61ca509ffb
Trang07102005 <ltttrang0710@gmail.com>
2024-10-15 15:11:53 +0700

    Add project files.

---
+++ 23dh114642_MyStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using _23dh114642_MyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh114642_MyStore.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        private MyStoreEntities db=new MyStoreEntities();
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            return View(products.ToList());
+        }
+    }
+}
